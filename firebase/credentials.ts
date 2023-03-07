ae9b528b1e0f54c6030095f47a7cfd2cc44bec17
Said Cueter <rattion@pop-os.localdomain>
2023-03-07 15:38:25 -0500

    Trying to fix deployment issues

--- firebase/credentials.ts
+++ firebase/credentials.ts
@@ -1,4 +1,4 @@
-export const firebaseCredentials = {
+export const firebaseCredentials = JSON.stringify({
   type: process.env.NEXT_PUBLIC_TYPE,
   project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
   private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
@@ -9,4 +9,4 @@ export const firebaseCredentials = {
   token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL
-}
+})
