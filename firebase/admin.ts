2386874cb6ed7fc82b1f27531a1d94beb6a8dc9d
Said Cueter <rattion@pop-os.localdomain>
2023-03-07 15:10:07 -0500

    Trying to fix deployment issues

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -5,7 +5,11 @@ import { firebaseCredentials } from './credentials'
 const serviceAccount = firebaseCredentials
 
 !admin.apps.length && admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+    privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID
+  })
 })
 
 export const firestore = admin.firestore()
