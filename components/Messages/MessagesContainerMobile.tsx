c96bbe9c6bad88d954a10efc8145ed0ee21f4036
Said Cueter <rattion@pop-os.localdomain>
2023-03-10 21:24:51 -0500

    Added feature to scroll to bottom when new message is received

--- components/Messages/MessagesContainerMobile.tsx
+++ components/Messages/MessagesContainerMobile.tsx
@@ -1,13 +1,19 @@
 
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface MessagesContainerMobileProps {
   children: React.ReactNode
 }
 export const MessagesContainerMobile = ({ children }: MessagesContainerMobileProps) => {
+  const chatContainerRef = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    if (chatContainerRef.current !== null) { chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight + 80 }
+  }, [children])
+
   return (
     <>
-      <section className='flex flex-col gap-3 h-5/6 overflow-y-scroll no-scrollbar px-1 relative max-h-screen pb-20'>
+      <section className='flex flex-col gap-3 h-3/4 overflow-y-scroll no-scrollbar px-1 relative pb-5' ref={chatContainerRef}>
         {children}
       </section>
     </>
