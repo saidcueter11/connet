9129f2736e999edde4c4e546a77acc8bb8a1e8d4
Said Cueter <rattion@pop-os.localdomain>
2023-03-11 01:22:01 -0500

    Changed layout for the messages card

--- components/Messages/MessagesCard.tsx
+++ components/Messages/MessagesCard.tsx
@@ -16,8 +16,8 @@ export const MessagesCard = ({ content, userId, imgUrl, createdAt }: MessagesCar
     <>
       {
         authUser?.uid === userId
-          ? <div className='bg-light-green grid rounded-2xl rounded-br-none shadow w-4/5 shadow-black/25 ml-auto p-4 pb-0'>
-              <p className='font-karla text-text-dark-green pb-3'>{content}</p>
+          ? <div className='bg-light-green grid rounded-2xl rounded-br-none shadow max-w-[75%] min-w-[40%] shadow-black/25 ml-auto p-4 pb-0'>
+              <p className='font-karla text-text-dark-green pb-3 w-fit'>{content}</p>
                 {
                   imgUrl &&
                     <a href={imgUrl} target='_blank' className='flex justify-center pb-2' rel="noreferrer">
@@ -26,15 +26,15 @@ export const MessagesCard = ({ content, userId, imgUrl, createdAt }: MessagesCar
                 }
               <time className='font-karla text-action-red pb-2 text-end'>{time}</time>
             </div>
-          : <div className='bg-dark-green grid rounded-2xl rounded-bl-none shadow w-4/5 shadow-black/25 p-4 pb-0'>
-              <p className='font-karla text-ligth-text-green pb-3'>{content}</p>
+          : <div className='bg-dark-green grid rounded-2xl rounded-bl-none min-w-[40%] shadow max-w-[75%] shadow-black/25 p-4 pb-0 mr-auto'>
+              <p className='font-karla text-ligth-text-green pb-3 w-fit'>{content}</p>
                 {
                   imgUrl &&
                     <a href={imgUrl} target='_blank' className='flex justify-center pb-2' rel="noreferrer">
                       <img src={imgUrl} className='rounded w-4/5 object-cover'/>
                     </a>
                 }
-              <time className='font-karla text-action-red-ligth pb-2'>{time}</time>
+              <time className='font-karla text-action-red-ligth pb-2 w-fit'>{time}</time>
             </div>
       }
 
