19fad138ac434d96e7b4a87324de41f39ac04513
Said Cueter <86256019+saidcueter11@users.noreply.github.com>
2023-03-31 14:03:37 -0500

    Added Id to send Button

--- components/Messages/SendMessageButton.tsx
+++ components/Messages/SendMessageButton.tsx
@@ -2,7 +2,7 @@ import { SendIcon } from '../Icons/SendIcon'
 
 export const SendMessageButton = ({ input }: {input?:string}) => {
   return (
-    <button className='rotate-45 disabled:opacity-50' disabled={input?.length === 0} type='submit'>
+    <button id='btnSendMessage' className='rotate-45 disabled:opacity-50' disabled={input?.length === 0} type='submit'>
       <SendIcon width={28} height={28} stroke='#EB6440' fill='none'/>
     </button>
   )
