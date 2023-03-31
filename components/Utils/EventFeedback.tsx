21ce9b0c4b7caf09e8316e7592a8052b9df037c7
Said Cueter <86256019+saidcueter11@users.noreply.github.com>
2023-03-31 12:47:40 -0500

    Applied - to Id in eventFeedback

--- components/Utils/EventFeedback.tsx
+++ components/Utils/EventFeedback.tsx
@@ -7,7 +7,7 @@ export const EventFeedback = ({ event, eventDescription }: EventFeedbackProps) =
   return (
     <>
       <div className={`absolute translate-y-3 rounded-lg p-4 bg-dark-green w-4/5 text-center font-karla text-ligth-text-green left-1/2 transform -translate-x-1/2 ${event ? 'opacity-100 z-20' : 'opacity-0 -z-10'} transition-opacity duration-300 shadow-xl`}>
-        <p id={eventDescription.join('-')}>{eventDescription}</p>
+        <p id={eventDescription.split(' ').join('-')}>{eventDescription}</p>
       </div>
     </>
   )
